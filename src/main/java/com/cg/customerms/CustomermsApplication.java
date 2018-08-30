8f987ac87b3ca259229ead6e7f679a7bcc6ca3cc
drishtihrao <drishtihrao@gmail.com>
2018-08-30 08:22:52 -0400

    Add files via upload

---
+++ src/main/java/com/cg/customerms/CustomermsApplication.java
@@ -0,0 +1,12 @@
+package com.cg.customerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomermsApplication.class, args);
+	}
+}
